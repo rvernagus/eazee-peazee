ef03b9ecf690abcc15cf027bb8f258b762b1b7e5
Ray Vernagus <r.vernagus@gmail.com>
2018-11-06 17:44:50 -0500

    Add Recipe model and Recipes prop

--- src/Pages/Recipes.cshtml.cs
+++ src/Pages/Recipes.cshtml.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EazeePeazee.Pages
 {
+    public class Recipe
+    {
+        public string Name { get; set; } = "";
+    }
+
     public class RecipesModel : PageModel
     {
+        public RecipesModel()
+        {
+            Recipes.Add(new Recipe { Name = "Roasted Chicken" });
+            Recipes.Add(new Recipe { Name = "Instant Pot Chicken Marinara" });
+        }
 
+        public IList<Recipe> Recipes { get; private set; } = new List<Recipe>();
     }
 }
