e5d4cb7fe68a10d4fa01b9ce850be37cbe8ccfba
Ray Vernagus <r.vernagus@gmail.com>
2018-11-06 08:25:28 -0500

    Fix namespace

--- src/Startup.cs
+++ src/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace src
+namespace EazeePeazee
 {
     public class Startup
     {
